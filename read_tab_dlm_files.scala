8c1fbbf0987a9b03667248c384731fa312909cb0
Puneet Tripathi <31759593+puneettripathi@users.noreply.github.com>
2017-11-25 12:47:35 +0530

    Update read_tab_dlm_files.scala

--- read_tab_dlm_files.scala
+++ read_tab_dlm_files.scala
@@ -9,12 +9,12 @@ var hiveCtx = new HiveContext(sc)
 val tabrdd = sc.textFile("PATH_to_TAB_DELIMITED_FILE_ON HDFS")
 
 // filter rdd remove headers
-val rdd = tabrdd.map(_.split("\t")).filter(_(0) != "DATECUTOFF").map(a => Row.fromSeq(a))
+val rdd = tabrdd.map(_.split("\t")).map(a => Row.fromSeq(a))
 // If your file may have continuous tabs and you need to treat them as separate columns, use line below 
 // val rdd = tabrdd.map(_.split("\t", -1)).filter(_(0) != "DATECUTOFF").map(a => Row.fromSeq(a))
 
 // header
-val header = tabrdd.map(_.split("\t")).filter(_(0) == "DATECUTOFF").first()
+val header = tabrdd.map(_.split("\t")).first()
 val schema = StructType(header.map(fieldName => StructField(fieldName.asInstanceOf[String],StringType,true)))
 
 // Create dataframe
